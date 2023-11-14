6bccd89c03e3e1f9f1733b5e22cbfce4e2e5a11d
Gyanendra <gyanendrat1234@gmail.com>
2023-11-14 16:17:14 +0530

    added some images for logo testing also added tmdb api key and tested it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+import {fetchDataFromAPI} from './utils/api';
+
 function App() {
+
+  useEffect(()=>{
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromAPI('/movie/popular').then((res) => {
+      console.log(res)
+    })
+  }
 
   return <div className="App">App</div>;
 }
