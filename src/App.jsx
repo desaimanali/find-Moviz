ef2c21437c5c46f30c041aade23f8279972af0ca
Gyanendra <gyanendrat1234@gmail.com>
2023-11-17 23:21:01 +0530

    completed heroBanner Section on Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -23,24 +23,29 @@ import PageNotFound from "./pages/404/PageNotFound";
 
 
 function App() {
-  // creating hook for redux
+  // creating hooks for redux
   const dispatch = useDispatch();
   const {url} = useSelector((state) => state.home);
 
   useEffect(()=>{
-    apiTesting();
+    fetchAPIConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromAPI('/movie/popular').then((res) => {
+  const fetchAPIConfig = () => {
+    fetchDataFromAPI('/configuration').then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
     })
   }
 
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:mediaType/:id" element={<Details />}/>
@@ -48,7 +53,7 @@ function App() {
             <Route path="/explore/:mediaType" element={<Explore />}/>
             <Route path="*" element={<PageNotFound />}/>
         </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
