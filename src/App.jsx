5a70b1f35f3d7c0a1bebacb476cc90cfcd216cdc
Gyanendra <gyanendrat1234@gmail.com>
2023-11-17 13:12:48 +0530

    completes setup of redux and also made necessary folders for components anad pages also completed setup of react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
+// importing react router dom
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+// importing API Methods
 import {fetchDataFromAPI} from './utils/api';
 
+// importing redux slices to use in react components
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+
+// importing all components and pages
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from './pages/home/Home';
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
+
 function App() {
+  // creating hook for redux
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home);
 
   useEffect(()=>{
     apiTesting();
@@ -11,11 +33,24 @@ function App() {
 
   const apiTesting = () => {
     fetchDataFromAPI('/movie/popular').then((res) => {
-      console.log(res)
+      console.log(res);
+      dispatch(getApiConfiguration(res));
     })
   }
 
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:mediaType/:id" element={<Details />}/>
+            <Route path="/search/:query" element={<SearchResult />}/>
+            <Route path="/explore/:mediaType" element={<Explore />}/>
+            <Route path="*" element={<PageNotFound />}/>
+        </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App
