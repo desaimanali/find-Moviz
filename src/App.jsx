678bcdd0c1f23f2dc6364c85dd60df48ab115ea6
Gyanendra <gyanendrat1234@gmail.com>
2023-11-19 13:57:20 +0530

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,73 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromAPI } from "./utils/api";
 
-// importing react router dom
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
-
-// importing API Methods
-import {fetchDataFromAPI} from './utils/api';
-
-// importing redux slices to use in react components
-import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration } from './store/homeSlice';
-
-// importing all components and pages
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Home from './pages/home/Home';
+import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 
-
 function App() {
-  // creating hooks for redux
-  const dispatch = useDispatch();
-  const {url} = useSelector((state) => state.home);
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  useEffect(()=>{
-    fetchAPIConfig();
-  }, []);
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-  const fetchAPIConfig = () => {
-    fetchDataFromAPI('/configuration').then((res) => {
-      console.log(res);
-      const url = {
-        backdrop: res.images.secure_base_url + "original",
-        poster: res.images.secure_base_url + "original",
-        profile: res.images.secure_base_url + "original",
-      };
-      dispatch(getApiConfiguration(url));
-    })
-  }
+    const fetchApiConfig = () => {
+        fetchDataFromAPI("/configuration").then((res) => {
+            console.log(res);
 
-  return (
-    <BrowserRouter>
-      {/* <Header /> */}
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:mediaType/:id" element={<Details />}/>
-            <Route path="/search/:query" element={<SearchResult />}/>
-            <Route path="/explore/:mediaType" element={<Explore />}/>
-            <Route path="*" element={<PageNotFound />}/>
-        </Routes>
-      {/* <Footer /> */}
-    </BrowserRouter>
-  );
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
+
+            dispatch(getApiConfiguration(url));
+        });
+    };
+
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromAPI(`/genre/${url}/list`));
+        });
+
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
+
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
